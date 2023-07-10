7fa120f8a94785c85d397fb849582e4c74104bce
Sagar Dhadke <peaceunique2003@gmail.com>
2023-07-10 09:24:06 +0530

    Predict First OutPut

---
+++ PreditFIrst.c
@@ -0,0 +1,18 @@
+//predict the output
+
+#include<stdio.h>
+
+int main(){
+    printf("nn\n\nnn\n");
+    printf("nn/n/nnn/n");
+    return 0;
+}
+
+
+
+    //My Answer 
+    /*
+    nn
+    
+    nn
+    nn/n/nnn/n*/
