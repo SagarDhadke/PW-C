1011cbb9be591ba705f6ce4a7bb06c373f63c077
Sagar Dhadke <peaceunique2003@gmail.com>
2023-07-10 11:54:00 +0530

    Arithmetic Operations

---
+++ Arithmetic Operations/Temperture.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    float f = 3.3;
+    float a =f *9/5+32;
+    printf("%f",a);
+    return 0;
+}
---
+++ Arithmetic Operations/float.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+//%d means int 
+// %d means Float
+
+int main(){
+    float x = 2.5;
+    float y = 2;
+    float z = x / y;
+    printf("%f",z);
+    return 0;
+}
