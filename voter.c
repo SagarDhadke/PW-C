6708ca866a56587059cfa2033cd23bc17f756af7
Sagar Dhadke <peaceunique2003@gmail.com>
2023-07-10 11:58:28 +0530

    Voting System Using C

---
+++ voter.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int age = 19;
+    if (age>=18){
+        printf("You Are Eligible for Vote");
+    }else{
+        printf("You Are not Eligible for vote");
+    }
+
+    return 0;
+    
+}
